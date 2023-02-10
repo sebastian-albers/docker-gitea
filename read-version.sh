4206240d4fb64a781699cf6a2ba6148ddc7974af
Sebastian Albers <seb.albers@gmail.com>
2023-02-10 11:39:34 +0100

    Renamed Github workflow to latest-release.

--- read-version.sh
+++ read-version.sh
@@ -3,5 +3,5 @@
 set -eou pipefail
 
 docker run --rm sebastianalbers/gitea:latest-$1 /app/gitea/gitea --version > version.tmp
-sed '/^Gitea version/!{q1}; s/^Gitea version \([0-9\.]*\) .*$/\1/' version.tmp
+sed '/^Gitea version/!{q1}; s/^Gitea version \([0-9\.]*\(\|\+\dev\)\)\( \|-\).*/\1/' version.tmp
 rm version.tmp

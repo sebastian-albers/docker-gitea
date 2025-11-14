e7eb2b18160b53bbcfd4072326c04367abe83feb
Sebastian Albers <seb.albers@gmail.com>
2025-11-14 16:54:09 +0100

    Fixed script for reading version.

--- read-version.sh
+++ read-version.sh
@@ -3,5 +3,5 @@
 set -eou pipefail
 
 docker run --rm sebastianalbers/gitea:latest /app/gitea/gitea --version > version.tmp
-sed '/^Gitea version/!{q1}; s/^Gitea version \([0-9\.]*\(\|\+\dev\)\)\( \|-\).*/\1/' version.tmp
+sed '/^[gG]itea version/!{q1}; s/^[gG]itea version \([0-9\.]*\(\|\+\dev\)\)\( \|-\).*/\1/' version.tmp
 rm version.tmp
